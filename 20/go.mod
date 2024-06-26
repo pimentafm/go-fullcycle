089a68dfceca355f7f58b6c4d413c7adbf818d4f
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-06-25 23:59:30 -0300

    Packages and modules

---
+++ 20/go.mod
@@ -0,0 +1,3 @@
+module go-mod-fc
+
+go 1.22.4
---
+++ 20/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"go-mod-fc/math"
+)
+
+func main() {
+	s := math.Sum(10, 20)
+
+	fmt.Printf("Result::: %v\n", s)
+}
