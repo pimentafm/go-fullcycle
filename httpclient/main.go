673e5fb21ea5a3a456b445551f6ce0df6e7fed6c
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-06 01:06:29 -0300

    Http Client

---
+++ httpclient/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Microsecond}
+	res, err := c.Get("http://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+
+}
