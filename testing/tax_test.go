d0f1a07a8941af3627bd528cf8f5376527e143a7
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-22 23:51:33 -0300

    Fuzzy

--- testing/tax_test.go
+++ testing/tax_test.go
@@ -43,3 +43,16 @@ func BenchmarkCalculateTax2(b *testing.B) {
 		CalculateTax2(500)
 	}
 }
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0, 30000.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Received %f but expected 0", result)
+		}
+	})
+}
